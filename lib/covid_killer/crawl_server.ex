099d771368fc1c5eb84ff58478e0a47a2b76812e
Jeff Cole <jeffreydonaldcole@gmail.com>
2021-02-16 09:04:30 -0500

    WIP

--- lib/covid_killer/crawl_server.ex
+++ lib/covid_killer/crawl_server.ex
@@ -1,27 +1,45 @@
 defmodule CovidKiller.CrawlServer do
   use GenServer
+  use Hound.Helpers
+
+  require Logger
 
   def start_link(_state) do
     GenServer.start_link(__MODULE__, nil)
   end
 
   def init(_state) do
-    schedule_initial()
+    schedule()
 
     {:ok, nil}
   end
 
   def handle_info(:crawl, state) do
-    # schedule_next()
+    Hound.start_session()
+
+    navigate_to("https://am-i-eligible.covid19vaccine.health.ny.gov/")
+
+    location = "Jones Beach - Field 3"
+
+    availablity =
+      :xpath
+      |> find_element("//*[text() = '#{location}']/following-sibling::td[2]")
+      |> inner_text()
+
+    Logger.info(availablity)
+
+    if !String.contains?(availablity, "No Appointments") do
+      System.cmd("say", ["Appointments available at #{location}"])
+    end
+
+    schedule()
 
     {:noreply, state}
+  after
+    Hound.end_session()
   end
 
-  defp schedule_initial do
-    Process.send_after(self(), :crawl, 1_000)
-  end
-
-  defp schedule_next do
+  defp schedule do
     Process.send_after(self(), :crawl, 30_000)
   end
 end
